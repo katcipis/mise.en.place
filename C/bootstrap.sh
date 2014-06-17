35e63587bd591f4270f9f22f826db9031eb04b70
Tiago Katcipis <tiago.katcipis@gmail.com>
2014-06-16 22:07:02 -0300

    -adding ssh port forwarding -adding vim stuff, not quite done yet.

--- C/bootstrap.sh
+++ C/bootstrap.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Installing stuff"
 apt-get update
-apt-get install -y pkg-config autogen autoconf build-essential git vim exuberant-ctags
+apt-get install -y pkg-config autogen autoconf build-essential git vim exuberant-ctags unzip
+
+echo "Installing some vim juice ;-)"
+wget https://github.com/katcipis/my.vim/archive/master.zip
+unzip master.zip
+cd my.vim-master
+make install
+make install-plugins
+cd ..
+rm -rf my.vim-master master.zip
