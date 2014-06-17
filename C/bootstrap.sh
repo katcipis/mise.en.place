cf8c50ce94163f46be5ed9391e45b9e0fde01946
Tiago Katcipis <tiago.katcipis@gmail.com>
2014-06-17 18:17:03 -0300

    -adding git stuff. -fixing vimrc installation.

--- C/bootstrap.sh
+++ C/bootstrap.sh
@@ -4,7 +4,13 @@ echo "Installing stuff"
 apt-get update
 apt-get install -y pkg-config autogen autoconf build-essential git vim exuberant-ctags unzip
 
+echo "Configuring git"
+git config --global user.name "Tiago Katcipis"
+git config --global user.email you@example.com
+git config --global push.default simple
+
 echo "Installing some vim juice ;-)"
+export HOME=/home/vagrant
 wget https://github.com/katcipis/my.vim/archive/master.zip
 unzip master.zip
 cd my.vim-master
@@ -12,3 +18,5 @@ make install
 make install-plugins
 cd ..
 rm -rf my.vim-master master.zip
+
+chown -R vagrant $HOME
