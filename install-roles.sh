c9fe28647e5a65796dee311e3b7018313d3191a8
Tiago Cesar Katcipis <tiago.katcipis@digitro.com.br>
2014-08-07 15:01:10 -0300

    adding zsh...why? because it is awesome :D

--- install-roles.sh
+++ install-roles.sh
@@ -3,5 +3,6 @@ rm -rf /etc/ansible/roles/katcipis.*
 mkdir -p /etc/ansible/roles
 
 cp -pr ./roles/vim /etc/ansible/roles/katcipis.workbench.vim
+cp -pr ./roles/zsh /etc/ansible/roles/katcipis.workbench.zsh
 cp -pr ./roles/git /etc/ansible/roles/katcipis.workbench.git
 cp -pr ./roles/ctools /etc/ansible/roles/katcipis.workbench.ctools
