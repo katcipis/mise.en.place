dad9343d5480cbdde39dddffc55cbc33d74f8303
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-08-18 20:09:09 -0300

    -using ansible for the base javascript environment

--- install-roles.sh
+++ install-roles.sh
@@ -6,3 +6,4 @@ cp -pr ./roles/vim /etc/ansible/roles/katcipis.workbench.vim
 cp -pr ./roles/zsh /etc/ansible/roles/katcipis.workbench.zsh
 cp -pr ./roles/git /etc/ansible/roles/katcipis.workbench.git
 cp -pr ./roles/ctools /etc/ansible/roles/katcipis.workbench.ctools
+cp -pr ./roles/jstools /etc/ansible/roles/katcipis.workbench.jstools
