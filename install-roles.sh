c1ab531db77d4d776c68825ef4191b346000983b
Tiago Cesar Katcipis <tiago.katcipis@digitro.com.br>
2014-09-05 09:40:24 -0300

    Makefile feels better :-)

--- install-roles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf /etc/ansible/roles/katcipis.*
-mkdir -p /etc/ansible/roles
-
-cp -pr ./roles/vim /etc/ansible/roles/katcipis.workbench.vim
-cp -pr ./roles/zsh /etc/ansible/roles/katcipis.workbench.zsh
-cp -pr ./roles/git /etc/ansible/roles/katcipis.workbench.git
-cp -pr ./roles/ctools /etc/ansible/roles/katcipis.workbench.ctools
-cp -pr ./roles/jstools /etc/ansible/roles/katcipis.workbench.jstools
