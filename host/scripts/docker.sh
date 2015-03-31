bd6e429b93456fa7ed078b083985053a7dbc0e41
Tiago Katcipis <tiago.katcipis@neoway.com.br>
2015-03-31 14:49:51 -0300

    adding docker compose

--- host/scripts/docker.sh
+++ host/scripts/docker.sh
@@ -5,3 +5,6 @@ curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
 sudo gpasswd -a $USER docker
 sudo service docker restart
+
+sudo apt-get install -y python-pip
+sudo pip install -U docker-compose
