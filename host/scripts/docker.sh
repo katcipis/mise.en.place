41680d776a1cd8fb2f43d397c64297bd3daa65bd
Tiago Katcipis <tiago.katcipis@neoway.com.br>
2015-04-01 17:14:18 -0300

    adding apparmor and aufs

--- host/scripts/docker.sh
+++ host/scripts/docker.sh
@@ -3,7 +3,12 @@
 echo "Installing docker"
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 
+echo "Adding user to docker group"
 sudo gpasswd -a $USER docker
+
+echo "Installing stuff that makes docker work properly :-)"
+sudo apt-get install apparmor aufs-tools
+
 sudo service docker restart
 
 sudo apt-get install -y python-pip
