842ce3896acf2f0f0d70f725ce72773d26548698
Tiago Katcipis <tiagokatcipis@gmail.com>
2015-07-26 23:44:17 -0300

    updating bootstrap to a specific version of docker

--- host/bootstrap.sh
+++ host/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
 ./scripts/common.sh
 ./scripts/vim.sh
 ./scripts/docker.sh
 ./scripts/python.sh
 ./scripts/go.sh
+./scripts/spotify.sh
--- host/scripts/docker.sh
+++ host/scripts/docker.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
+DOCKER_VERSION=1.6.2
+
+# Add the repository to your APT sources
+echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
+# Then import the repository key
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Install docker
+apt-get update
+
 echo "Installing docker"
-curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+apt-get install -y lxc-docker-$DOCKER_VERSION
 
 echo "Adding user to docker group"
-sudo gpasswd -a $USER docker
+gpasswd -a $USER docker
 
 echo "Installing stuff that makes docker work properly :-)"
-sudo apt-get install -y apparmor aufs-tools
+apt-get install -y apparmor aufs-tools curl
 
-sudo service docker restart
+
+service docker restart
 
 echo "Installing docker compose"
-sudo apt-get install -y python-pip
-sudo pip install -U docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "=== docker done ==="
