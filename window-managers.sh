bf01d73ed37303228eb9ea9330abc6ca6e3eebce
Tiago Katcipis <tiagokatcipis@gmail.com>
2015-09-05 23:47:13 -0300

    adding Rio

--- window-managers.sh
+++ window-managers.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Installing I3"
 sudo pacman --noconfirm -S i3-wm i3status i3lock dmenu
+
+echo "Installing Rio"
+cd $HOME
+git clone git@github.com:katcipis/plan9port.git
+cd plan9port
+./INSTALL
