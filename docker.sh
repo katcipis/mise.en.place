e600922927ff1481a5726e6e4ad98dcaadc7b684
Tiago Katcipis <tiagokatcipis@gmail.com>
2015-09-07 19:48:11 -0300

    adding docker and X install

--- docker.sh
+++ docker.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+
 set -e
 
-DOCKER_VERSION=1.6.2
+echo "Installing docker"
+sudo pacman -S docker
+
+echo "Adding user to docker group"
+sudo gpasswd -a $USER docker
+
+echo "Enabling docker"
+sudo systemctl enable docker
 
 echo "Installing docker compose"
-curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-echo "Done"
+DOCKER_COMPOSE_VERSION=1.4.0
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo ""
+echo "=== Done, is we !!! ==="
