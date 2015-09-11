c8afb770160183dc66d580063504d914cbc7de8a
Tiago Katcipis <tiago.katcipis@neoway.com.br>
2015-09-11 11:32:39 +0000

    adding script to install spotify :D

--- spotify.sh
+++ spotify.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -e
 
-# 1. Add the Spotify repository signing key to be able to verify downloaded packages
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+cd /tmp
 
-# 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+git clone https://aur.archlinux.org/libgcrypt15.git
+cd libgcrypt15
+makepkg -sri
+cd -
 
-# 3. Update list of available packages
-apt-get update
-
-# 4. Install Spotify
-apt-get install -y spotify-client
+git clone https://aur.archlinux.org/spotify.git
+cd spotify
+makepkg -sri
