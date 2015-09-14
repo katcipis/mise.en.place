6e502133d3cc1feb0b4bf908eece297e9dbcd903
Tiago Katcipis <tiago.katcipis@neoway.com.br>
2015-09-14 14:09:17 +0000

    installing vim version with X integration

--- vim.sh
+++ vim.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-#echo "Setting up Python goodness"
-#apt-get install -y pylint python-setuptools python-dev
-#easy_install -U pip
-#pip install --upgrade pyflakes 
-#pip install --upgrade pep8 
-#pip install --upgrade ipython
+cd /tmp
+
+echo "Removing vi"
+sudo pacman -R vi
+
+echo "Installing Vim"
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-x --enable-pythoninterp=yes --enable-python3interp=yes
+make
+sudo make install
+sudo ln -s  /usr/local/bin/vim /usr/bin/vi
 
 echo "Installing my vim"
 git clone git@github.com:katcipis/my.vim.git
