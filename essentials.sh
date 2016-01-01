2f32e1e3c9cb2aa524ccd4691b769858b163f3e2
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-01 16:18:43 -0200

    lets install pulseaudio

--- essentials.sh
+++ essentials.sh
@@ -8,8 +8,8 @@ git config --global push.default simple
 git config --global user.name "Tiago Katcipis"
 git config --global user.email "tiagokatcipis@gmail.com"
 
-echo "Installing Alsa"
-sudo pacman --noconfirm -S alsa-utils alsa-tools
+echo "Installing Sound Stuff"
+sudo pacman --noconfirm -S alsa-utils alsa-tools pulseaudio
 
 echo "Configure time properly"
 sudo ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
