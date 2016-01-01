097afb3496c293f313da0b5189ee02cad90cf7ff
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-01 01:29:38 -0200

    removing crap that never worked :-(

--- x.sh
+++ x.sh
@@ -3,5 +3,3 @@ set -e
 
 sudo pacman --noconfirm -S xorg-server xorg-server-utils xf86-input-synaptics \
     xf86-input-keyboard xf86-input-mouse xorg-utils xorg-xinit
-
-sudo cp ./xorg.conf.d/10-evdev.conf /etc/X11/xorg.conf.d/10-evdev.conf
