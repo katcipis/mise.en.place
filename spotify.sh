20e6d8cd438ebc4cc5587b0a6de77c9a65f6acd3
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-03 17:53:09 -0200

    still not working properly :-(

--- spotify.sh
+++ spotify.sh
@@ -3,11 +3,13 @@ set -e
 
 cd /tmp
 
+rm -rf libgcrypt15
 git clone https://aur.archlinux.org/libgcrypt15.git
 cd libgcrypt15
 makepkg -sri
 cd -
 
+rm -rf spotify
 git clone https://aur.archlinux.org/spotify.git
 cd spotify
 makepkg -sri
--- vim.sh
+++ vim.sh
@@ -3,10 +3,8 @@ set -e
 
 cd /tmp
 
-echo "Removing vi"
-sudo pacman -R vi
-
 echo "Installing Vim"
+rm -rf vim
 git clone https://github.com/vim/vim.git
 cd vim
 ./configure --with-x --enable-pythoninterp=yes --enable-python3interp=yes
@@ -15,6 +13,8 @@ sudo make install
 sudo ln -s  /usr/local/bin/vim /usr/bin/vi
 
 echo "Installing my vim"
+cd -
+rm -rf my.vim
 git clone https://github.com/katcipis/my.vim.git
 cd my.vim
 make bootstrap
