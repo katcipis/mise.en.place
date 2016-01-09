41c7a6b8dbbd78517a4e2d9f0a3e812b7cc172eb
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-09 19:07:15 -0200

    adding vlc

--- Makefile
+++ Makefile
@@ -7,11 +7,5 @@ system:
 	./scripts/i3.sh
 	./scripts/i3-cfg.sh
 
-guard-%:
-	@ if [ "${${*}}" = "" ]; then \
-                echo "Variable '$*' not set"; \
-                exit 1; \
-        fi
-
-i3cfg: guard-user
-	./scripts/i3-cfg.sh $(user)
+i3cfg:
+	./scripts/i3-cfg.sh
--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -14,8 +14,8 @@ git config --global push.default simple
 git config --global user.name "Tiago Katcipis"
 git config --global user.email "tiagokatcipis@gmail.com"
 
-echo "Installing Sound Stuff"
-sudo apt-get install -y audacious
+echo "Installing Media Stuff"
+sudo apt-get install -y audacious vlc
 
 echo "Golang Support"
 sudo apt-get install -y golang-go
