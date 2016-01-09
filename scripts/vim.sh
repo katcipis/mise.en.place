ec834830e5d4bb5a9e7c70f1f3499a725cc7da2b
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-09 18:10:15 -0200

    removing unused stuff and migrating vim

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 set -e
 
-cd /tmp
-
 echo "Installing Vim"
-rm -rf vim
-git clone https://github.com/vim/vim.git
-cd vim
-./configure --with-x --enable-pythoninterp=yes --enable-python3interp=yes
-make
-sudo make install
-sudo ln -s  /usr/local/bin/vim /usr/bin/vi
+sudo apt-get install -y vim-gnome
 
 echo "Installing my vim"
-cd -
+cd /tmp
 rm -rf my.vim
 git clone https://github.com/katcipis/my.vim.git
 cd my.vim
