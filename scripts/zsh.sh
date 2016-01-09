ddba068cad222b4b88f19b1c7f2540b43f1bb4e2
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-01-09 18:50:36 -0200

    fix zsh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing zsh and my zshrc"
-sudo pacman -S --noconfirm zsh
+sudo apt-get install zsh
 cd /tmp
 git clone https://github.com/katcipis/my.zsh.git
 git clone https://github.com/katcipis/my.fonts.git
