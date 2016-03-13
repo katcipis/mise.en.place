94fcc8def8326c18f94c2eab1a9398ce3c84ece1
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-03-12 21:42:24 -0300

    Fix i3 install on Mint

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -8,8 +8,8 @@ sudo apt-get install -y apt-file
 sudo apt-file update
 
 echo "Installing git and other stuff"
-sudo apt-get install -y openssh-server curl openvpn cmake xclip git unrar org-mode
-sudo systemctl disable openvpn.service
+sudo apt-get install -y openssh-server curl openvpn cmake xclip git unrar
+# sudo systemctl disable openvpn.service
 
 git config --global push.default simple
 git config --global user.name "Tiago Katcipis"
--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-sudo apt-get install -y i3 i3status i3lock i3blocks
+sudo apt-get install -y i3 i3status i3lock suckless-tools xfce4-appfinder
