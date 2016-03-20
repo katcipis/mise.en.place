55d8495e08a53e53ccc8e78ec231ff160162cc16
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-03-19 23:19:54 -0300

    Fix Golang install

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -8,4 +8,6 @@ echo "Install Go "$GO_VERSION" at "$GOROOT
 
 cd /tmp
 wget https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
-tar -C $GOROOT -xvf go1.6.linux-amd64.tar.gz
+tar -xvf go1.6.linux-amd64.tar.gz
+mkdir -p $GOROOT
+mv go/* $GOROOT
