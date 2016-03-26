2805c4c2b6e329b7a0d3ded1ff11098d1e2995c9
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-03-26 18:40:53 -0300

    YESS

--- scripts/color-themes.sh
+++ scripts/color-themes.sh
@@ -4,6 +4,7 @@ set -o errexit
 sudo pacman --noconfirm -S dconf
 
 cd /tmp
+rm -rf gnome-terminal-colors-solarized
 git clone git@github.com:Anthony25/gnome-terminal-colors-solarized.git
 cd gnome-terminal-colors-solarized
 ./install.sh
