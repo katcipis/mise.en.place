adddfda5a3270f212ef463886c202c0539dc33b8
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-03-26 16:56:46 -0300

    Not sure if i3-gnome is required

--- scripts/window-managers.sh
+++ scripts/window-managers.sh
@@ -8,8 +8,8 @@ sudo pacman --noconfirm -S gnome
 echo "Installing I3"
 sudo pacman --noconfirm -S  i3-wm i3status i3lock
 
-cd /tmp
-rm -rf i3-gnome
-git clone https://aur.archlinux.org/i3-gnome.git
-cd i3-gnome
-makepkg -sri
+#cd /tmp
+#rm -rf i3-gnome
+#git clone https://aur.archlinux.org/i3-gnome.git
+#cd i3-gnome
+#makepkg -sri
