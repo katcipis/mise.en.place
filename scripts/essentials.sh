dbf60c43d69b8301901ec2f3df62d3a095b0edd5
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-03-26 22:18:33 -0300

    Add unrar

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing git and other basic stuff"
-sudo pacman --noconfirm -S git tig openssh sudo gnome-terminal curl dmenu cmake xclip
+sudo pacman --noconfirm -S git tig openssh sudo gnome-terminal curl dmenu cmake xclip unrar
 
 git config --global push.default simple
 git config --global user.name "Tiago Katcipis"
