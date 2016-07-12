f75726e569b478be041e3616ababbd2929130e86
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-07-11 21:31:46 -0300

    Add Gstreamer

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -28,3 +28,6 @@ sudo pacman --noconfirm -S alsa-utils alsa-tools pulseaudio pavucontrol
 
 echo "Installing image/diagrams tools"
 sudo pacman --noconfirm -S imagemagick dia
+
+echo "Install Gstreamer"
+sudo pacman --noconfirm -S gstreamer gst-libav gst-plugins-bad  gst-plugins-good gst-plugins-base gst-plugins-ugly
