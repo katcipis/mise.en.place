17c9bfca6278a8237d1e3e46b4edbd3d82e74a51
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-08-13 13:51:21 -0300

    Update to Go 1.7

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -2,12 +2,14 @@
 set -o errexit
 set -o nounset
 
-GO_VERSION=1.6
+GO_VERSION="1.7rc6"
 
 echo "Install Go "$GO_VERSION" at "$GOROOT
 
 cd /tmp
 wget https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
 tar -xvf go$GO_VERSION.linux-amd64.tar.gz
+
+rm -rf $GOROOT
 mkdir -p $GOROOT
 mv ./go/* $GOROOT
