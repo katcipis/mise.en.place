b2830e870c7bc9bef4700fe4702c844f39f64748
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-10-05 12:07:16 -0300

    Add xcwd to have better terminal creation :-)

--- scripts/window-managers.sh
+++ scripts/window-managers.sh
@@ -8,8 +8,5 @@ sudo pacman --noconfirm -S gnome
 echo "Installing I3"
 sudo pacman --noconfirm -S  i3-wm i3status i3lock
 
-#cd /tmp
-#rm -rf i3-gnome
-#git clone https://aur.archlinux.org/i3-gnome.git
-#cd i3-gnome
-#makepkg -sri
+echo "Installing xcwd"
+./scripts/aur.sh xcwd-git
