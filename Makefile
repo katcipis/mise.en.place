fa93a4629cb2ad07923024637974b7b9bc26b0e4
Tiago Katcipis <tiagokatcipis@gmail.com>
2016-10-22 17:56:32 -0200

    Remove Gnome

--- Makefile
+++ Makefile
@@ -16,3 +16,6 @@ system:
 
 iso:
 	sudo nash setupiso.nsh
+
+usb:
+	sudo dd bs=4M if=$(iso) of=$(dev) status=progress && sync
--- scripts/window-managers.sh
+++ scripts/window-managers.sh
@@ -2,9 +2,6 @@
 set -e
 set -u
 
-echo "Installing gnome"
-sudo pacman --noconfirm -S gnome
-
 echo "Installing I3"
 sudo pacman --noconfirm -S  i3-wm i3status i3lock
 
