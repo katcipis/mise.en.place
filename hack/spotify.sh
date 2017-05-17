cf9ce25996ceb2aefd4c4dba2b7e630c4b972ba4
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-05-17 20:59:19 -0300

    Fix spotify install

--- hack/spotify.sh
+++ hack/spotify.sh
@@ -2,7 +2,11 @@
 
 set -o errexit
 
+gpg --recv-keys --keyserver hkp://pgp.mit.edu D9C4D26D0E604491
+gpg --recv-keys 5CC908FDB71E12C2
+
 ./hack/aur.sh libopenssl-1.0-compat
 ./hack/aur.sh libcurl-compat
+./hack/aur.sh libcurl-openssl-1.0
 ./hack/aur.sh package-query
 ./hack/aur.sh  spotify
