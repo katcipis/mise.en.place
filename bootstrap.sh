367782767207612d65b791e96619852233a90ab1
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-10-13 20:35:10 -0300

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,15 @@ export GOROOT=$HOME/go
 export GOPATH=$HOME/workspace/go
 export PATH=$PATH":"$GOPATH"/bin:"$GOROOT"/bin"
 
+hack/essentials.sh
+hack/x.sh
+hack/fonts.sh
+hack/golang.sh
+hack/window-managers.sh
+hack/terminal.sh
+hack/browsers.sh
+hack/kitty-cfg.sh
+hack/docker.sh
 hack/zsh.sh
 hack/vim.sh
 hack/spotify.sh
