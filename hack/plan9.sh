bce745aa426d49cf679ef31d75858e21a5a39e1a
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-03-20 20:35:13 -0300

    Fix xinitrc to init rio

--- hack/plan9.sh
+++ hack/plan9.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+installdir="/usr/local/plan9"
 workdir=`mktemp -d`
 old=`pwd`
 cd $workdir
 echo "working on:"$workdir
-wget https://github.com/9fans/plan9port/archive/master.zip
+wget https://github.com/madlambda/plan9port/archive/master.zip
 unzip master.zip
-sudo mv plan9port-master /usr/local/plan9
+sudo rm -rf $installdir
+sudo mv plan9port-master $installdir
 rm -rf $workdir
-cd /usr/local/plan9
+cd $installdir
 sudo ./INSTALL
