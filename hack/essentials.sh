42b5380611ce19b0808ca6edd694728ed2c7e4c0
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-03-29 15:50:49 +0100

    Fix timezone

--- hack/essentials.sh
+++ hack/essentials.sh
@@ -11,7 +11,7 @@ git config --global user.name "Tiago Katcipis"
 git config --global user.email "tiagokatcipis@gmail.com"
 
 echo "Configure time properly"
-sudo ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+sudo ln -fs /usr/share/zoneinfo/Europe/Rome /etc/localtime
 sudo timedatectl set-ntp true
 
 echo "Configure keyboard layout"
