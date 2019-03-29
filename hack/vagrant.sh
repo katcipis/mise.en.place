5543e85a6a6fe3ac7e1c7a6827e21e3ca65c3a27
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-03-29 13:39:50 +0100

    Remove clutter

--- hack/vagrant.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e 
-
-echo "Installing Vagrant"
-sudo pacman --noconfirm -S virtualbox virtualbox-host-dkms net-tools
-sudo usermod -a -G vboxusers $USER
-sudo pacman --noconfirm -S vagrant
