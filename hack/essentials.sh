c6b777350559bcd4b9612b42cb4b742fafb7642a
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-05-07 21:12:27 -0300

    Add vlc

--- hack/essentials.sh
+++ hack/essentials.sh
@@ -32,7 +32,7 @@ echo "Installing image/diagrams tools"
 sudo pacman --noconfirm -S imagemagick dia
 
 echo "Install Gstreamer"
-sudo pacman --noconfirm -S gstreamer gst-libav gst-plugins-bad  gst-plugins-good gst-plugins-base gst-plugins-ugly
+sudo pacman --noconfirm -S gstreamer gst-libav gst-plugins-bad  gst-plugins-good gst-plugins-base gst-plugins-ugly vlc
 
 echo "Install Monitoring tools"
 sudo pacman --noconfirm -S sysdig iotop iftop htop sysstat
