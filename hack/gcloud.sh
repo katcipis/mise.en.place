bb3df16f5dbcaf8f6c6fcb23af6877a543bc6516
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-09-25 17:35:33 +0200

    Fix gcloud install

--- hack/gcloud.sh
+++ hack/gcloud.sh
@@ -10,6 +10,6 @@ rm -rf $installdir
 mkdir -p $installdir
 cd $installdir
 
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$(version)-linux-x86_64.tar.gz
-tar zxvf google-cloud-sdk-$(version)-linux-x86_64.tar.gz google-cloud-sdk
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-linux-x86_64.tar.gz
+tar zxvf google-cloud-sdk-${version}-linux-x86_64.tar.gz google-cloud-sdk
 ./google-cloud-sdk/install.sh
