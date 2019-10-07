25adb2f1268a27dbadb969361143f584b4910bf6
Tiago Katcipis <tiago.katcipis@fromatob.com>
2019-10-07 11:10:46 +0200

    Refactor tools

--- tools/fixres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -o errexit
-
-# Hack for edid fucked up monitors
-xrandr --newmode "1920x1080x60.0" 148.50  1920 2008 2052 2200  1080 1084 1089 1125 +hsync +vsync
-xrandr --addmode HDMI-1 "1920x1080x60.0"
-xrandr --output HDMI-1 --mode "1920x1080x60.0"
--- tools/net-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-interface=enp1s0
-sudo systemctl start dhcpcd@${interface}
