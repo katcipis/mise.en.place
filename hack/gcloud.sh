35b7daf5accc26617824d608f7452bfbecf3b1e1
Tiago Katcipis <tiago.katcipis@fromatob.com>
2019-12-04 16:12:37 +0100

    Update gcloud install

--- hack/gcloud.sh
+++ hack/gcloud.sh
@@ -4,14 +4,15 @@ set -o errexit
 set -o nounset
 
 workdir=$(mktemp -d)
-installdir=$HOME/.local/gcloud
+basedir=$HOME/.local
+installdir=$basedir/google-cloud-sdk
 version="272.0.0"
 
 cd ${workdir}
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-linux-x86_64.tar.gz
 tar zxvf google-cloud-sdk-${version}-linux-x86_64.tar.gz
 rm -rf ${installdir}
-mv google-cloud-sdk ${installdir}
+mv google-cloud-sdk ${basedir}
 cd ${installdir}
 ./install.sh
 rm -rf ${workdir}
