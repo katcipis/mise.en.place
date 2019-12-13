374ef3ffc276cebd9380da987783fa3e19310b0d
Tiago Katcipis <tiagokatcipis@gmail.com>
2019-12-13 15:00:38 +0100

    Remove st and update arch

--- hack/terminal.sh
+++ hack/terminal.sh
@@ -4,23 +4,3 @@ set -o nounset
 set -o errexit
 
 sudo pacman -S --noconfirm kitty
-
-projname=st-master
-projpath=/tmp/st
-filepath=$projpath/st.zip
-
-rm -rf $projpath
-mkdir -p $projpath
-
-wget https://github.com/katcipis/st/archive/master.zip -O $filepath
-cd $projpath
-unzip $filepath
-cd $projname
-
-echo "Building st"
-make
-sudo make install
-cd -
-
-echo "Done, cleaning up"
-rm -rf $projpath
