aa0f63809075ee15f5cba0cadd49eec144f99964
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-12-08 18:08:49 +0100

    final user namespace ID mapping config

--- hack/podman.sh
+++ hack/podman.sh
@@ -10,4 +10,5 @@ sudo pacman --noconfirm -S podman slirp4netns fuse-overlayfs
 # FROM: https://github.com/containers/podman/blob/master/docs/tutorials/rootless_tutorial.md
 sudo touch /etc/subuid
 sudo touch /etc/subgid
-sudo usermod --add-subuids 260000-325536 --add-subgids 260000-325536 "${USER}"
+sudo usermod --add-subuids 1001-655370 --add-subgids 1001-655370 "${USER}"
+sudo usermod --add-subuids 1000-1 --add-subgids 1000-1 root
