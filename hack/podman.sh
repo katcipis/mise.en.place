87246d51570d5cb1966733edb27310527347ee12
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-12-08 17:41:08 +0100

    Add some user id stuff, not working yet

--- hack/podman.sh
+++ hack/podman.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "installing and setting up rootless podman"
+
 set -o nounset
 set -o errexit
 
 sudo pacman --noconfirm -S podman slirp4netns fuse-overlayfs
-sudo pacman --noconfirm -S podman-docker
+
+# FROM: https://github.com/containers/podman/blob/master/docs/tutorials/rootless_tutorial.md
+sudo touch /etc/subuid
+sudo touch /etc/subgid
+sudo usermod --add-subuids 260000-325536 --add-subgids 260000-325536 "${USER}"
