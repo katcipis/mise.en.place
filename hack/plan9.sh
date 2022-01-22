e3de25493fda19d6531f9da6fcc21c722f99e0a1
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-01-22 19:20:50 +0100

    chore: improve plan9port install

--- hack/plan9.sh
+++ hack/plan9.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+
 installdir="/usr/local/plan9"
 workdir=`mktemp -d`
-old=`pwd`
-cd $workdir
+
 echo "working on:"$workdir
+echo "downloading plan9port"
+
+cd $workdir
 wget https://github.com/madlambda/plan9port/archive/master.zip
 unzip master.zip
+
+echo "build and install plan9port"
+
 sudo rm -rf $installdir
 sudo mv plan9port-master $installdir
 rm -rf $workdir
 cd $installdir
 sudo ./INSTALL
+
+echo "done"
