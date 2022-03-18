f4b16dd7ba9fd4070e96803618e2658e02f31ed3
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-03-18 21:47:28 +0100

    improve script, thanks to @i4ki

--- hack/golang.sh
+++ hack/golang.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+
 set -o errexit
 set -o nounset
 
 version="1.18"
-installdir=$HOME/.local/go
+installdir="${HOME}/.local/go"
 
-echo "Install Go "$version" at "$installdir
+echo "Install Go ${version} at ${installdir}"
 
 cd /tmp
-wget https://storage.googleapis.com/golang/go$version.linux-amd64.tar.gz
-tar -xvf go$version.linux-amd64.tar.gz
+wget "https://storage.googleapis.com/golang/go${version}.linux-amd64.tar.gz"
+tar -xvf "go${version}.linux-amd64.tar.gz"
 
-rm -rf $installdir
-mkdir -p $installdir
-mv ./go/* $installdir
+rm -rf "${installdir}"
+mkdir -p "${installdir}"
+mv ./go/* "${installdir}"
