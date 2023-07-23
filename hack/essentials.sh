9e026c292e45b82ad886f09bec9fca7795cafaa4
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-07-23 21:04:07 +0200

    feat: add missing stuff and removing useless stuff

--- hack/essentials.sh
+++ hack/essentials.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Installing essential tools"
 sudo pacman --noconfirm -S nethogs lsof gnu-netcat openssh sudo curl dmenu fzf evince neovim npm
-sudo pacman --noconfirm -S make cmake xclip unrar slock numlockx wget fakeroot unzip python-pip
+sudo pacman --noconfirm -S make cmake xclip unrar slock numlockx wget fakeroot unzip python-pip pkg-config
 sudo pacman --noconfirm -S dnsutils linux-headers squashfs-tools cdrkit xorriso dosfstools ntfs-3g
 
 echo "Configure time properly"
