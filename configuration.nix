823845150ceaf9ca48935e307e1e9619094fa61c
Tiago Katcipis <tiagokatcipis@gmail.com>
2025-12-12 23:25:54 +0100

    update config again

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,6 @@
     virtualisation.podman.enable = true;
 
     networking.hostName = "katz";
-    networking.wireless.enable = true;
     networking.networkmanager.enable = true;
 
     environment.systemPackages = with pkgs; [
@@ -31,13 +30,45 @@
         podman
         kubectl
         google-cloud-sdk
+        google-chrome
         tailscale
-        chromium
         spotify
         slack
     ];
 
     hardware.graphics.enable = true;
+    hardware.nvidia = {
+        modesetting.enable = true;
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+        # of just the bare essentials.
+        powerManagement.enable = false;
+
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of 
+        # supported GPUs is at: 
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+        # Only available from driver 515.43.04+
+        open = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+     # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
 
     services.xserver.enable = true;
     services.xserver.videoDrivers = [ "nvidia" ];
@@ -51,6 +82,7 @@
         enable = true;
         enableSSHSupport = true;
     };
+    programs.firefox.enable = true;
 
 
     time.timeZone = "Europe/Berlin";
